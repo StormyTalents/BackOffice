dd9fe3f15b97efef51efa54494c0c0f8feb31107
StormyTalents <stormintalent@gmail.com>
2022-09-06 06:58:12 -0500

    feat: add routes

---
+++ src/routes/constants/index.js
@@ -0,0 +1,30 @@
+export const ROUTES = {
+  SIGN_IN: "/login",
+  DASHBOARD: "/dashboard",
+  PRACTICES: "/practices",
+  CUSTOMERS: "/customers",
+  INTEGRATION: "/integration",
+  DETECTIVES: "/detectives",
+  PRODUCTS: "/products",
+  ORDERS: "orders",
+  REPORT: "report_order",
+};
+
+export const ROLES = {
+  ADMIN: {
+    label: "admin",
+    id: 1,
+  },
+  CUSTOMER: {
+    label: "customer",
+    id: 2,
+  },
+  DETECTIVE: {
+    label: "detective",
+    id: 3,
+  },
+  CHIEF: {
+    label: "chief",
+    id: 4,
+  },
+};
